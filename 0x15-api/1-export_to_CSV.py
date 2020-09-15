eacdea3cf0767baf8aa93fcdf83c03efe479ccac
dondropo <alejandroruscamoreno@gmail.com>
2020-09-15 03:50:38 +0000

    [Fix]: re-upload task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """Export data to CSV format"""
 from sys import argv
-import requests as r
+import requests
 import csv
 
 
 if __name__ == '__main__':
     userId = argv[1]
-    user = r.get("https://jsonplaceholder.typicode.com/users/{}".
-                 format(userId), verify=False).json()
-    tdl = r.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                format(userId), verify=False).json()
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(userId), verify=False).json()
+    tdl = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                       format(userId), verify=False).json()
 
 with open("{}.csv".format(userId), 'w', newline='') as csvfile:
         taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
